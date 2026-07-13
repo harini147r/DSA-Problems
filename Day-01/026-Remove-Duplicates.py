af2ba5f52260d51ac85485c6f5f33f834e51b808
harini147r <harinir.bds24@rathinam.in>
2026-07-13 12:34:36 +0530

    Add the Remove Duplicates-LC-026 Solution

---
+++ Day-01/026-Remove-Duplicates.py
@@ -0,0 +1,24 @@
+class Solution:
+    def removeDuplicates(self, nums):
+        if len(nums) == 0:
+            return 0
+
+        i = 0
+
+        for j in range(1, len(nums)):
+            if nums[j] != nums[i]:
+                i += 1
+                nums[i] = nums[j]
+
+        return i + 1
+
+
+# Driver Code
+nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+
+sol = Solution()
+k = sol.removeDuplicates(nums)
+
+print("Number of unique elements:", k)
+print("Modified Array:", nums)
+print("Unique Elements:", nums[:k])
