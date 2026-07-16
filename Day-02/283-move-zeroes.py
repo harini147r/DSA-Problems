c2124e2fb2256109978b5cb0e02fb5be9da045c7
harini147r <harinir.bds24@rathinam.in>
2026-07-16 15:56:05 +0530

    Started Day-02 with completing  LC-011 and LC-283 Solutions

---
+++ Day-02/283-move-zeroes.py
@@ -0,0 +1,8 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+       count = 0
+       for i in range(len(nums)):
+           if nums[i]!=0:
+               nums[i],nums[count]=nums[count],nums[i]
+               count+=1
+       return nums
